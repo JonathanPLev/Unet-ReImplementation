57777fabcc66a893027d44e5435bbe978f29f720
JonathanPLev <jplevitsky@ucdavis.edu>
2025-11-21 13:55:24 -0800

    feat(u-net): implement model architecture

--- implementation.py
+++ implementation.py
@@ -19,6 +19,7 @@ NUM_WORKERS = 4
 BATCH_SIZE = 1  # batch size detailed in the paper
 FLIP_PROBABILITY = 0.5
 MOMENTUM_TERM = 0.99  # detailed in paper
+NUM_OUTPUT_CHANNELS = 2  # detailed in paper
 
 # TODO: map the image with pixel-wise loss weight so it learns border images. formula 2
 # TODO: implement weight initialization as detailed in the paper
@@ -37,7 +38,9 @@ def transforms(image, mask, crop_size=256):
 
     image = F.resize(image, size=[512, 512], interpolation=InterpolationMode.BILINEAR)
 
-    mask = (mask > 0).astype(np.uint8) # because dataset also has set-up for multi class segmentation.
+    mask = (mask > 0).astype(
+        np.uint8
+    )  # because dataset also has set-up for multi class segmentation.
     mask = torch.tensor(
         mask, dtype=torch.long
     )  # convert to tensor + make correct dtype
@@ -93,19 +96,155 @@ class SegmentationDataset(Dataset):
 
 class Net(nn.Module):
     def __init__(self):
-        # TODO: implement model architecture
+        # TODO: double check model architecture implementation (Jon)
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(3 * 32 * 32, 10)
+        self.conv1 = nn.Conv2d(
+            in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=0
+        )
+        self.conv2 = nn.Conv2d(
+            in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=0
+        )
+        self.conv3 = nn.Conv2d(
+            in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=0
+        )
+        self.conv4 = nn.Conv2d(
+            in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=0
+        )
+        self.conv5 = nn.Conv2d(
+            in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=0
+        )
+        self.conv6 = nn.Conv2d(
+            in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=0
+        )
+        self.conv7 = nn.Conv2d(
+            in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=0
+        )
+        self.conv8 = nn.Conv2d(
+            in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=0
+        )
+        self.conv9 = nn.Conv2d(
+            in_channels=512, out_channels=1024, kernel_size=3, stride=1, padding=0
+        )
+        self.conv10 = nn.Conv2d(
+            in_channels=1024, out_channels=1024, kernel_size=3, stride=1, padding=0
+        )
+
+        # up convolution
+        self.up1 = nn.ConvTranspose2d(
+            in_channels=1024, out_channels=512, stride=2, kernel_size=2
+        )
+        self.conv11 = nn.Conv2d(
+            in_channels=1024, out_channels=512, stride=1, kernel_size=3, padding=0
+        )
+        self.conv12 = nn.Conv2d(
+            in_channels=512, out_channels=512, stride=1, kernel_size=3, padding=0
+        )
+
+        self.up2 = nn.ConvTranspose2d(
+            in_channels=512, out_channels=256, stride=2, kernel_size=2
+        )
+        self.conv13 = nn.Conv2d(
+            in_channels=512, out_channels=256, stride=1, kernel_size=3, padding=0
+        )
+        self.conv14 = nn.Conv2d(
+            in_channels=256, out_channels=256, stride=1, kernel_size=3, padding=0
+        )
+
+        self.up3 = nn.ConvTranspose2d(
+            in_channels=256, out_channels=128, stride=2, kernel_size=2
+        )
+        self.conv15 = nn.Conv2d(
+            in_channels=256, out_channels=128, stride=1, kernel_size=3, padding=0
+        )
+        self.conv16 = nn.Conv2d(
+            in_channels=128, out_channels=128, stride=1, kernel_size=3, padding=0
+        )
+
+        self.up4 = nn.ConvTranspose2d(
+            in_channels=128, out_channels=64, stride=2, kernel_size=2
+        )
+        self.conv17 = nn.Conv2d(
+            in_channels=128, out_channels=64, stride=1, kernel_size=3, padding=0
+        )
+        self.conv18 = nn.Conv2d(
+            in_channels=64, out_channels=64, stride=1, kernel_size=3, padding=0
+        )
+
+        self.out1 = nn.Conv2d(
+            in_channels=64,
+            out_channels=NUM_OUTPUT_CHANNELS,
+            stride=1,
+            kernel_size=1,
+            padding=0,
+        )
 
     def forward(self, x):
         # TODO: implement model
-        x = x.view(-1, 3 * 32 * 32)
-        x = F.relu(self.fc1(x))
-        return x
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        skip1 = x
+        x = self.pool(x)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        skip2 = x
+        x = self.pool(x)
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        skip3 = x
+        x = self.pool(x)
+        x = F.relu(self.conv7(x))
+        x = F.relu(self.conv8(x))
+        skip4 = x
+        x = self.pool(x)
+        x = F.relu(self.conv9(x))
+        x = F.relu(self.conv10(x))
+        # end of down sampling
+
+        # up sampling 1
+        x = self.up1(x)
+        _, _, H, W = x.shape
+        skip4_cropped = torchvision.transforms.CenterCrop([H, W])(skip4)
+        x = torch.cat([x, skip4_cropped], dim=1)
+        x = F.relu(self.conv11(x))
+        x = F.relu(self.conv12(x))
+
+        # up sampling 2
+        x = self.up2(x)
+        _, _, H, W = x.shape
+        skip3_cropped = torchvision.transforms.CenterCrop([H, W])(skip3)
+        x = torch.cat([x, skip3_cropped], dim=1)
+        x = F.relu(self.conv13(x))
+        x = F.relu(self.conv14(x))
+
+        # up sampling 3
+        x = self.up3(x)
+        _, _, H, W = x.shape
+        skip2_cropped = torchvision.transforms.CenterCrop([H, W])(skip2)
+        x = torch.cat([x, skip2_cropped], dim=1)
+        x = F.relu(self.conv15(x))
+        x = F.relu(self.conv16(x))
+
+        # up sampling 4
+        x = self.up4(x)
+        _, _, H, W = x.shape
+        skip1_cropped = torchvision.transforms.CenterCrop([H, W])(skip1)
+        x = torch.cat([x, skip1_cropped], dim=1)
+        x = F.relu(self.conv17(x))
+        x = F.relu(self.conv18(x))
+
+        # output layer
+        x = self.out1(x)
+        return x  # logits
+
 
 # TODO: define train loop
 def train_u_net(train_loader):
     net = Net()
+
+
+# TODO: implement loss function
+def loss_func(net):
+    nn.CrossEntropyLoss()
 
 
 if __name__ == "__main__":
